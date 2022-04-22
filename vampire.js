cef17a7fa4c8d0ab9174adf0a49ed90ae1d109c9
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-22 14:23:10 -0400

    seeing if committing lets me switch to traversal branch

--- vampire.js
+++ vampire.js
@@ -10,22 +10,39 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
+    return this.offspring;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    let numOfOffspring = 0;
+    for (let child of this.offspring) {
+      numOfOffspring++;
+    }
+    return numOfOffspring;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numAway = 0
 
+    while (this.creator) {
+      numAway++;
+      this.creator = this.creator.creator;
+    }
+
+    return numAway;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) {
+      return false;
+    }
+    return true;
   }
 
   /** Stretch **/
@@ -36,7 +53,7 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    
   }
 }
 
